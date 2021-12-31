a96ae5294bf26f68217319db0253a9eefb611b0d
Andrew Repp <71296468+ReppCodes@users.noreply.github.com>
2021-12-31 17:00:59 -0600

    working single thread implementation with todos

--- src/dose_parser.rs
+++ src/dose_parser.rs
@@ -1,57 +1,30 @@
+use std::collections::{HashMap, HashSet};
 use std::error::Error;
 use std::path::PathBuf;
-use std::collections::HashMap;
 
-use chrono::NaiveDate;
+use chrono::{Duration, NaiveDate};
 use serde::Deserialize;
-
-#[derive(Debug)]
-pub struct PatientCalendar {
-    drug_name: String,
-    coverage_date: NaiveDate,
-    covered_bool: bool
-}
-
-impl PatientCalendar{
-    fn new() -> PatientCalendar {
-        PatientCalendar {
-            drug_name: String,
-            coverage_date: NaiveDate,
-            covered_bool: bool
-        }
-    }
-}
-
 #[derive(Debug)]
 pub struct Patient {
     pub patient_id: String,
-    pub adherence: f64,
-    pub drug_list: Vec<String>,
-    pub given_doses: Vec<Dose>,
-    pub shifted_calendar: PatientCalendar
+    pub overall_adherence: f64,
+    pub drug_lvl_adherence: HashMap<String, f64>,
+    pub drug_list: HashSet<String>,
+    pub given_doses: HashMap<String, Vec<Dose>>,
+    pub shifted_calendar: HashMap<String, HashMap<NaiveDate, bool>>,
 }
 
 impl Patient {
     fn new(patient_id: String) -> Patient {
-        Patient{
+        Patient {
             patient_id: patient_id,
-            adherence: 0.0,
-            drug_list: Vec::new(),
-            given_doses: Vec::new(),
-            shifted_calendar: PatientCalendar::new()
+            overall_adherence: 0.0,
+            drug_lvl_adherence: HashMap::new(),
+            drug_list: HashSet::new(),
+            given_doses: HashMap::new(),
+            shifted_calendar: HashMap::new(),
         }
     }
-    fn create_calendar(&self) -> () {
-
-        self.shifted_calendar;
-    }
-}
-
-pub fn create_calendar(patient: Patient) -> Result<PatientCalendar, Box<dyn Error>> {
-    let dummy_date: NaiveDate = NaiveDate::from_ymd(1999, 01, 01);
-    let patient_cal: PatientCalendar = PatientCalendar { patient_id: "johnsmith".to_string(), drug_name: "drugname".to_string(), coverage_date:  dummy_date};
-
-    Ok(patient_cal)
 }
 
 // By default, struct field names are deserialized based on the position of
@@ -68,7 +41,7 @@ pub struct Dose {
 #[allow(dead_code)]
 mod mmddyyyy_fmt {
     // format to bring string-based dates in and out of the Dose struct
-    use chrono::{NaiveDate};
+    use chrono::NaiveDate;
     use serde::{self, Deserialize, Deserializer, Serializer};
 
     const FORMAT: &'static str = "%m/%d/%Y";
@@ -100,28 +73,147 @@ mod mmddyyyy_fmt {
         D: Deserializer<'de>,
     {
         let s = String::deserialize(deserializer)?;
-        NaiveDate::parse_from_str(&s, FORMAT)
-            .map_err(serde::de::Error::custom)
+        NaiveDate::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
     }
 }
 
 pub fn parse_doses(file_in: PathBuf) -> Result<HashMap<String, Patient>, Box<dyn Error>> {
     let csv_reader = csv::Reader::from_path(file_in);
     let mut patient_map: HashMap<String, Patient> = HashMap::new();
-    for record in csv_reader.unwrap().deserialize() {
+    for record in csv_reader
+        .expect("Please select a single, correctly formatted, CSV file.")
+        .deserialize()
+    {
         let dose: Dose = record?;
         let patient_id: String = dose.patient_id.clone();
 
-        if !patient_map.contains_key(&patient_id){
-            let mut new_pat: Patient = Patient::new(patient_id.clone());
-            new_pat.given_doses.push(dose);
+        if !patient_map.contains_key(&dose.patient_id) {
+            // New patient
+            let mut new_pat: Patient = Patient::new(dose.patient_id.clone());
+            new_pat
+                .given_doses
+                .insert(dose.drug_name.clone(), Vec::new());
+            new_pat.drug_list.insert(dose.drug_name.clone());
+            new_pat
+                .given_doses
+                .get_mut(&dose.drug_name)
+                .unwrap()
+                .push(dose);
             patient_map.insert(patient_id, new_pat);
+        } else if !patient_map
+            .get(&dose.patient_id)
+            .unwrap()
+            .given_doses
+            .contains_key(&dose.drug_name)
+        {
+            // Patient already exist, but new drug found
+            // Add drug to patient
+            (*patient_map.get_mut(&dose.patient_id).unwrap())
+                .given_doses
+                .insert(dose.drug_name.clone(), Vec::new());
+            (*patient_map.get_mut(&dose.patient_id).unwrap())
+                .drug_list
+                .insert(dose.drug_name.clone());
+
+            // Add dose to drug
+            (*patient_map.get_mut(&dose.patient_id).unwrap())
+                .given_doses
+                .get_mut(&dose.drug_name)
+                .unwrap()
+                .push(dose);
+        } else {
+            // Patient already exists and has drug.  Add dose.
+            (*patient_map.get_mut(&dose.patient_id).unwrap())
+                .given_doses
+                .get_mut(&dose.drug_name)
+                .unwrap()
+                .push(dose);
         }
-        else {
-            (*patient_map.get_mut(&dose.patient_id).unwrap()).given_doses.push(dose);
-        }
-        
     }
     Ok(patient_map)
 }
 
+// Core program logic moved down here away from structs and ser/deser for clarity
+impl Patient{
+    pub fn create_calendar(&mut self) -> () {
+        for (drug_name, dose_list) in &mut self.given_doses {
+            // sort given_doses by date
+            dose_list.sort_by(|a, b| a.fill_date.cmp(&b.fill_date));
+
+            // iterate through doses, generating shifted start and stop dates
+            let mut first_start_dt = NaiveDate::from_ymd(2900, 1, 1);
+            let mut last_end_dt = NaiveDate::from_ymd(1900, 1, 1);
+            let mut prior_end_dt = NaiveDate::from_ymd(1900, 1, 2);
+            let mut covered_dates: HashSet<NaiveDate> = HashSet::new();
+            for dose in dose_list {
+                let mut start_dt: NaiveDate = dose.fill_date;
+
+                // shift start date to handle early refills
+                if start_dt < prior_end_dt{
+                    start_dt = prior_end_dt + Duration::days(1);
+                }
+
+                // calculate end date off of shifted start date
+                // TODO check if we need a -1 here, to account for pill taken on day of fill
+                let end_dt: NaiveDate = start_dt + Duration::days(dose.days_supply.into());
+                prior_end_dt = end_dt;
+
+                // generate set of all covered days for this dose, pushed into set for given drug
+                let mut dt = start_dt;
+                while dt <= end_dt{
+                    covered_dates.insert(dt);
+                    dt = dt + Duration::days(1);
+                }
+
+                // bookkeeping on start/end dates for patient analysis window
+                // TODO should these be passed in by user, optionally?  
+                    // this doesn't allow for eligiblity if it's available, but tbh it never is....
+                if end_dt > last_end_dt{
+                    last_end_dt = end_dt;
+                }
+                if start_dt < first_start_dt{
+                    first_start_dt = start_dt;
+                }
+            }
+
+            // generate full range of days for patient analysis window
+            let mut all_dates: HashSet<NaiveDate> = HashSet::new();
+            let mut dt = first_start_dt;
+            while dt <= last_end_dt{
+                all_dates.insert(dt);
+                dt = dt + Duration::days(1);
+            }
+
+            let mut coverage_cal: HashMap<NaiveDate, bool> = HashMap::new();
+            for date in all_dates{
+                if covered_dates.contains(&date){
+                    coverage_cal.insert(date, true);
+                }
+                else{
+                    coverage_cal.insert(date, false);
+                }
+            }
+            self.shifted_calendar.insert(drug_name.clone(), coverage_cal);
+        }
+    }
+    pub fn calculate_pdc(&mut self) -> () {
+        let mut overall_total_days: HashSet<NaiveDate> = HashSet::new();
+        let mut overall_covered_days: HashSet<NaiveDate> = HashSet::new();
+        for (drug_name, calendar) in &mut self.shifted_calendar {
+            let mut numerator = 0;
+            let mut denominator = 0;
+            for (date, covered) in calendar{
+                if *covered == true{
+                    numerator += 1;
+                    overall_covered_days.insert(*date);
+                }
+                denominator += 1;
+                overall_total_days.insert(*date);
+                let drug_adh: f64 = numerator as f64 / denominator as f64;
+                self.drug_lvl_adherence.insert(drug_name.clone(), drug_adh);
+            }
+        }
+        self.overall_adherence = overall_covered_days.len() as f64 / overall_total_days.len() as f64;
+        
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-mod dose_parser;
 mod date_shifter;
-mod pdc;
+mod dose_parser;
 mod file_selector;
+mod pdc;
 
 fn main() {
-    
     // user must select csv with claims information
-
+    // single file for now. multiple files in the future?  possibly also eligibility files?
     let file_in = file_selector::select_file().unwrap();
-    let patients = dose_parser::parse_doses(file_in);
-    for (id, patient) in patients.unwrap().iter_mut() {
-        dose_parser::create_calendar(patient);
-    
+    let patients = dose_parser::parse_doses(file_in).unwrap();
+
+    for (_id, mut patient) in patients {
+        patient.create_calendar();
+        patient.calculate_pdc();
+        println!("{:?}", patient.overall_adherence);
+        println!("{:?}", patient.drug_lvl_adherence);
     }
 
-    // TODO -- see how to add error handling to the dose parser
-    // if let Err(err) = example() {
-    //     println!("error running example: {}", err);
-    //     process::exit(1);
-    // }
-
-    // Perform date shifting
-
-    // calculate PDC on shifted dates
-
-    // print results to stdout
+    // TODO
+    // 1. add more tests.  multiple patients, multiple drugs, etc
+    // 2. add multi-threading.  see TODOs scattered around
+    // 3. figure out how to dump this out to a file.  simple as second nfd selector and serialize out?
+    // 4. add support for multiple imported CSVs
+    // 5. add data checks on imports
+        // check for duplicate doses
+        // check for ..... ?
 }
